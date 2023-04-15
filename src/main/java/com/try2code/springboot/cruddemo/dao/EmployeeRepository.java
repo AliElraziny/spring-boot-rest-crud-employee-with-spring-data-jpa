2e69813fe742a27d0048a4484262649c64f71a2d
aly elraziny <alyelraziny@gmail.com>
2023-04-15 14:49:03 +0200

    first commit

--- src/main/java/com/try2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/try2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.try2code.springboot.cruddemo.dao;
 
 import com.try2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee , Integer> {
 
 
